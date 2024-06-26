8434839c22a306696857825502726f12edac9797
Dodong2 <carlseighartaliodegmail.com>
2024-06-26 11:34:18 +0800

    6th commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import Board from './components/Board'
-
+import { lazy, Suspense } from 'react'
 import './App.css'
-
 function App() {
+
+const Board = lazy(() => import('./components/Board'))
 
   return (
     <>
+    <Suspense fallback={<div className=''></div>}>
     <Board/>
+    </Suspense>
     </>
   )
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
-
-import { FaCartArrowDown } from "react-icons/fa";
-import { IoHomeSharp } from "react-icons/io5";
-import { IoWallet } from "react-icons/io5";
 import { BsCartCheckFill } from "react-icons/bs";
 
 import img1 from '/public/img1.svg'
 import img2 from '/public/img2.svg'
+
+
 
 const Board = () => {
     const [showModal1, setModal1] = useState<boolean>(false)
@@ -21,65 +19,29 @@ const Board = () => {
     return (
         <>
             <div className='main-container'>
-                <nav className='p-2 nav bg-dark fixed-top z-3'>
-                    <ul className="nav nav-underline gap-3">
-                        <li className="nav-item">
-                            <a className="nav-link text-light fs-4 text d-flex justify-content-center align-items-center gap-2" aria-current="page" href="#home"><IoHomeSharp className='fs-6 text' /> Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-light fs-4 text d-flex justify-content-center align-items-center gap-2" href="#cart"><FaCartArrowDown className='fs-6 text' /> Cart</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-light fs-4 text d-flex justify-content-center align-items-center gap-2" href="#expen"><IoWallet className='fs-6 text' /> Expenses</a>
-                        </li>
-                    </ul>
-                </nav>
                 {/*----------Contents----------*/}
+                <div className='img-holder'>
+                <div className='images'>
+                <img src={img2} />
+               <img src={img1} />
+                </div>
+                </div>
                 <div className='contents'>
-                    <section id='home'>
-                        <div className='first-container'>
-                            <div className='img1'><img src={img1} /></div>
-                            <div className='btn1'>
-                                <img src={img2} /><br />
-                                <button onClick={() => setModal1(!showModal1)}><BsCartCheckFill />ADD</button>
-                            </div></div>
-                    </section><hr />
                     {/*----------Tables----------*/}
-                    <section id='cart'><br />
-                        <div className='table-set'>
-                            <h1>Cart</h1>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Items</th>
-                                        <th>Prices</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Egg</td>
-                                        <td>P: 10</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Egg</td>
-                                        <td>P: 10</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>Egg</td>
-                                        <td>P: 10</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </section><hr />
                     <section id='expen'>
                         <div className='table-set'>
-                            <h1>Expences</h1><br />
+                            <main>
+                                <div className='title'>
+                                    <h1>Expenses</h1>
+                                </div>
+                            </main>
+                            <br />
                             <table>
+                                <thead>
+                                    <tr>
+                                        <th colSpan={3}>Expenses this month: 1000</th>
+                                    </tr>
+                                </thead>
                                 <thead>
                                     <tr>
                                         <th>#</th>
@@ -106,6 +68,12 @@ const Board = () => {
                                 </tbody>
                             </table>
                         </div>
+                    </section>
+                    <section id='home'>
+                        <div className='first-container'>
+                            <div className='btn1 position-fixed bottom-0 end-0 z-3 p-5'>
+                                <button onClick={() => setModal1(!showModal1)}><BsCartCheckFill />ADD</button>
+                            </div></div>
                     </section>
                 </div>
                 {/*----------Modal----------*/}
